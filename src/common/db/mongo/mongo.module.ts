49341dc33babcbded78d2c2e370be8f56b394584
Dũng Nguyễn Tuấn <dungnt@thomi.com.vn>
2024-07-18 17:05:10 +0700

    update template

--- src/common/db/mongo/mongo.module.ts
+++ src/common/db/mongo/mongo.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [MongooseModule.forRootAsync({
+    useFactory: () => ({
+      uri: process.env.MONGO_URI,
+    }),
+  })],
 })
 export class MongoModule {}
--- src/common/my-jwt/my-jwt.service.ts
+++ src/common/my-jwt/my-jwt.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MyJwtService {}
+export class MyJwtService {
+  contructor() {}
+}
