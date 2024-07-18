df7a6d5acf7cb50dc2082169ae242258e66609f5
dungnt@thomi.com.vn <dungnt@thomi.com.vn>
2024-07-18 14:55:26 +0700

    chore: Add JWT module for token generation and validation

--- src/common/my-jwt/my-jwt.module.ts
+++ src/common/my-jwt/my-jwt.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MyJwtService } from './my-jwt.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   providers: [MyJwtService],
 })
 export class MyJwtModule {}
