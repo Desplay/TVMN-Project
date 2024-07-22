8a5c44b386b29113cc60f180a4a1bad4039953b2
desplayshido <desplayshido@outlook.com>
2024-07-22 09:32:37 +0700

    fix UI

--- src/common/db/mongo/mongo.module.ts
+++ src/common/db/mongo/mongo.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [MongooseModule.forRootAsync({
-    useFactory: () => ({
-      uri: process.env.MONGO_URI,
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_URI,
+      }),
     }),
-  })],
+  ],
 })
 export class MongoModule {}
